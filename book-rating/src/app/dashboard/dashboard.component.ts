dc88ccf5499bf4e8b5131a29229fa023fa096e60
Ferdinand Malcher <ferdinand@malcher.media>
2022-11-09 12:25:24 +0100

    Interface Book und Buchliste

---
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../shared/book';
+
+@Component({
+  selector: 'br-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  books: Book[] = [];
+
+  constructor() {
+    this.books = [
+      {
+        isbn: '12345',
+        title: 'Angular',
+        description: 'Das große Praxisbuch',
+        rating: 5,
+        price: 42.9
+      },
+      {
+        isbn: '54321',
+        title: 'Vue.js',
+        description: 'Das grüne Framework',
+        rating: 3,
+        price: 39.9
+      }
+    ];
+  }
+
+  ngOnInit(): void {
+  }
+
+}
+
