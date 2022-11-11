c2b2e5e735c34fb077072b1c2f10c48ce01dcde8
Ferdinand Malcher <ferdinand@malcher.media>
2022-11-11 21:30:46 +0100

    RxJS-Playground hinzugefügt

---
+++ rxjs-playground/src/app/exercises/exercises.ts
@@ -0,0 +1,12 @@
+import { Exercises } from './types';
+
+export const exercisesList: Exercises = [
+    { name: 'Creating Observables', folder: 'creating' },
+    { name: 'Window resize: Observables from events', folder: 'fromevent' },
+    { name: 'Multicasting with Subjects', folder: 'multicast' },
+    { name: 'Error handling', folder: 'error-handling' },
+    { name: 'How to unsubscribe and avoid memory leaks', folder: 'unsubscribe' },
+    { name: 'Higher Order Observables with concatMap, mergeMap, switchMap, exhaustMap', folder: 'higherorder' },
+    { name: '[EXTRA] Chat: Merging Observables', folder: 'chat' },
+    { name: '[EXTRA] Drag and drop', folder: 'dragdrop' },
+];
