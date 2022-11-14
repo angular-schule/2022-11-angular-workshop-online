a888d845bf14bd4d7322a7e4104d21e8c48befae
Ferdinand Malcher <ferdinand@malcher.media>
2022-11-14 11:03:53 +0100

    Routing: Weiterleitung, Links, Detailseite

--- book-rating/src/app/books/book-details/book-details.component.ts
+++ book-rating/src/app/books/book-details/book-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Book } from '../shared/book';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'br-book-details',
@@ -7,7 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookDetailsComponent implements OnInit {
 
-  constructor() { }
+  book?: Book;
+
+  constructor(private route: ActivatedRoute, private bs: BookStoreService) {
+    // Synchroner Weg / PULL
+    // const isbn = this.route.snapshot.paramMap.get('isbn'); // books/:isbn
+
+    // Asynchroner Weg / PUSH
+    this.route.paramMap.subscribe(params => {
+      const isbn = params.get('isbn')!; // Non-Null Assertion
+      this.bs.getSingle(isbn).subscribe(book => {
+        this.book = book;
+      });
+    });
+
+  }
 
   ngOnInit(): void {
   }
