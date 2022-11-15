8d6153f32f014391985d809a280858141dd82686
Ferdinand Malcher <ferdinand@malcher.media>
2022-11-15 11:36:53 +0100

    Vorbereitung für die Suche

--- book-rating/src/app/books/book-search/book-search.component.ts
+++ book-rating/src/app/books/book-search/book-search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { BookStoreService } from '../shared/book-store.service';
 
 @Component({
   selector: 'br-book-search',
@@ -7,7 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookSearchComponent implements OnInit {
 
-  constructor() { }
+  searchControl = new FormControl('', { nonNullable: true });
+
+  constructor(private bs: BookStoreService) {
+    this.searchControl.valueChanges
+      .subscribe(e => {
+        console.log(e);
+      })
+  }
 
   ngOnInit(): void {
   }
